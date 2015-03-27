2576ac7b82df2d54fb84b043c0a89f7a9babb3b0
Fabrice Colas <fcolas@ubivar.com>
2015-03-27 09:16:42 +0100

    Add test cases for pagination, order, limit, gt, gte, lt, lte

--- test/Resources/spec.js
+++ test/Resources/spec.js
@@ -121,6 +121,8 @@ describe("Resources", function() {
         var example   = require("../data/"+resource)
           , idResource
 
+
+
         it("Should create and return a resource", function(done){
           ubivar[resource].create(example[0], function(err, res){
             if(err) done(err)
@@ -176,16 +178,120 @@ describe("Resources", function() {
           })
         })
 
-        it("Should list the resources", function(done){
-          var params  = {"start_after": 0, "end_before": 1}
-            , nList   = params.end_before - params.start_after
+        describe("Should list and paginate", function(){
+          var ids         = []
 
-          ubivar[resource].list(params, function(err, res){
-            if(res.status === 200 && res.data.length === nList){
+          before(function(done){
+            var example   = require("../data/"+resource)
+            this.timeout(5000)
+            ubivar[resource].create(example[0], function(err, res){
+              if(err) return done(err)
+              ids.push(res.data[0].id)
+              ubivar[resource].create(example[0], function(err, res){
+                if(err) return done(err)
+                ids.push(res.data[0].id)
+                ubivar[resource].create(example[0], function(err, res){
+                  if(err) return done(err)
+                  ids.push(res.data[0].id)
+                  done()
+                })
+              })
+            })
+          })
+
+          it("Should limit retrieved resources to N=1", function(done){
+            var nLimit = 1
+            ubivar[resource].list({limit:nLimit}, function(err, res){
+              if(err) done(err) 
+              else if(res.data.length === nLimit) done()
+              else done(new Error("Should 'limit' list to N="+nLimit+" resources")) 
+            })
+          })
+
+          it("Should limit retrieved resources to N=2", function(done){
+            var nLimit = 2
+            ubivar[resource].list({limit:nLimit}, function(err, res){
+              if(err) done(err) 
+              else if(res.data.length === nLimit) done()
+              else done(new Error("Should 'limit' list to N="+nLimit+" resources")) 
+            })
+          })
+
+          it("Should 'start_after' when paginating", function(done){
+            var nLimit = 2
+            ubivar[resource].list({"start_after": ids[0], "limit":nLimit}, function(err, res){
+              if(err) return done(err)    
+              if(res.data.length !== nLimit) return done(new Error("Should return N="+nLimit))
+              var returnedIds   = _.pluck(res.data, "id")
+              if(_.contains(returnedIds, ids[0])){ 
+                return done(new Error("Should not return 'starting_after' id"))
+              }
               done()
-            } else {
-              done(new Error("Did not return a list of " + resource))
-            }
+            })
+          })
+
+          it("Should 'start_after' and 'end_before' when paginating", function(done){
+            var nLimit = 1
+            ubivar[resource].list({"end_before": ids[2], "start_after":ids[0]}, function(err, res){
+              if(err) return done(err)    
+              if(res.data.length !== nLimit) return done(new Error("Should return N="+nLimit))
+              var returnedIds   = _.pluck(res.data, "id")
+              if(_.contains(returnedIds, ids[1])) return done()
+              else return done(new Error("Should not return 'starting_after' id"))
+            })
+          })
+
+          it("Should list ids greater than (gt) a given id", function(done){
+            var nLimit  = 2
+              , object  = {"id":{"gt":ids[0]}, "limit":nLimit}
+            ubivar[resource].list(object, function(err, res){
+              if(err) return done(err)    
+              if(res.data.length !== nLimit) return done(new Error("Should return N="+nLimit))
+              var returnedIds   = _.pluck(res.data, "id")
+              if(_.contains(returnedIds, ids[1]) && _.contains(returnedIds, ids[2])){ 
+                return done()
+              } 
+              return done(new Error("Should contain the right ids"))
+            })
+          })
+
+          it("Should list and order DESC the ids that are less than (lt) an id", function(done){
+            var nLimit = 2
+            ubivar[resource].list({"id":{"lt":ids[2]}, "order":"-id", "limit":nLimit}, function(err, res){
+              if(err) return done(err)    
+              if(res.data.length !== nLimit) return done(new Error("Should return N="+nLimit))
+              var returnedIds   = _.pluck(res.data, "id")
+              if(_.contains(returnedIds, ids[0]) && _.contains(returnedIds, ids[1]) && !_.contains(returnedIds,ids[2])){ 
+                return done()
+              } 
+              return done(new Error("Should contain the right ids"))
+            })
+          })
+
+          it("Should list ids greater than or equal (gte) to a given id", function(done){
+            var nLimit = 3
+            ubivar[resource].list({"id":{"gte":ids[0]}, "limit":nLimit}, function(err, res){
+              if(err) return done(err)    
+              if(res.data.length !== nLimit) return done(new Error("Should return N="+nLimit))
+              var returnedIds   = _.pluck(res.data, "id")
+              if(_.contains(returnedIds, ids[0]) && _.contains(returnedIds, ids[1]) && _.contains(returnedIds, ids[2])){ 
+                return done()
+              } 
+              return done(new Error("Should contain the right ids"))
+            })
+          })
+
+          it("Should list and order DESC the ids that are less than or equal (lte) an id", function(done){
+            var nLimit = 3
+            ubivar[resource].list({"id":{"lte":ids[2]}, "order":"-id", "limit":nLimit}, function(err, res){
+              if(err) return done(err)    
+              if(res.data.length !== nLimit) return done(new Error("Should return N="+nLimit))
+              var returnedIds   = _.pluck(res.data, "id")
+              if(_.contains(returnedIds, ids[0]) && _.contains(returnedIds, ids[1]) && _.contains(returnedIds,ids[2])){ 
+                return done()
+              } 
+              return done(new Error("Should contain the right ids"))
+            })
           })
         })
       })
