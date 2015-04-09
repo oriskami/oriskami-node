9c32c405499038fcf58dff9ae664934a618de7c8
Fabrice Colas <fcolas@ubivar.com>
2015-04-09 10:20:25 +0200

    Update test cases of fx api endpoint after data structure change

--- test/Resources/spec.js
+++ test/Resources/spec.js
@@ -53,10 +53,10 @@ describe("Resources", function() {
           ubivar.set("timeout", 20000)
 
           ubivar.me.retrieve(function(gotError, res){
-            if( isAuthorized &&  gotError) done((new Error(message)))
+            if( isAuthorized &&  gotError){console.log(gotError); done((new Error(message)))}
             if( isAuthorized && !gotError) done()
             if(!isAuthorized &&  gotError) done()
-            if(!isAuthorized && !gotError) done((new Error(message)))
+            if(!isAuthorized && !gotError){console.log(gotError); done((new Error(message)))}
           })
         })
       }
@@ -112,17 +112,28 @@ describe("Resources", function() {
         ubivar.fx.retrieve(function(err, res){
           if(err){
             done(err)
-          } else if(!(res.status === 200 && res.data.length === 1)){
+          } else if(!(res.status === 200 && res.data.length > 1)){
             done(new Error("Did not return FX of the day "))
           } else{
-            var fx  = res.data[0]
-            if(fx.cur_from !== "EUR"){
-              done(new Error("Returned 'cur_from' does not default to EUR"))
-            } else if(fx.cur_to !== "USD"){
-              done(new Error("Returned 'cur_to' does not default to USD"))
-            } else if((new Date()-new Date(fx.date))/(1000*60*60*24) > 2){
+            var cur_from        = _.pluck(res.data, "cur_from")
+              , cur_to          = _.pluck(res.data, "cur_to"  )
+              , rates           = _.pluck(res.data, "rate"    )
+              , dates           = _.pluck(res.data, "date"    )
+              , cur_exp         = ["AUD","BGN","BRL","CAD","CHF","CNY","CZK","DKK","GBP","HKD","HRK","HUF","IDR","ILS","INR","JPY","KRW","MXN","MYR","NOK","NZD","PHP","PLN","RON","RUB","SEK","SGD","THB","TRY","USD","ZAR"]
+              , cur_obs         = _.union(cur_from, cur_to)
+              , hasAllCurrencies= _.difference(cur_exp, cur_obs).length === 0
+              , validNumbers    = _.reduce(rates, function(memo, rate){return memo && _.isNumber(rate)}, true)
+              , validDates      = _.reduce(dates, function(memo, date){return memo && (new Date() - new Date(date)/(1000*60*60*24)) > 2}, true)
+
+            if(!hasAllCurrencies){
+              var msg           = "cur exp:"+JSON.stringify(cur_exp)+"\ncur to:"+JSON.stringify(cur_obs)+"\ndelta:"+JSON.stringify(cur_delta)
+              console.log(msg)
+              done(new Error())
+            } else if(!validDates){
+              console.log(dates)
               done(new Error("Latest FX should be less than one day old"))
-            } else if(!_.isNumber(fx.rate)) {
+            } else if(!validNumbers) {
+              console.log(rates)
               done(new Error("Did not return a number"))
             } else {
               done()
@@ -156,21 +167,23 @@ describe("Resources", function() {
       })
 
       it("Should return fx rate for a specific date", function(done){
-        var date  = "2015-01-01"
+        var date_exp  = "2015-01-01"
 
-        ubivar.fx.list({"date":date}, function(err, res){
+        ubivar.fx.list({"date":date_exp}, function(err, res){
           if(err){
             done(err)
-          } else if(!(res.status === 200 && res.data.length === 1)){
-            done(new Error("Did not return FX of the day for two custom currencies"))
+          } else if(!(res.status === 200 && res.data.length > 1)){
+            done(new Error("Did not return any FX"))
           } else {
-            var fx  = res.data[0]
-            if((new Date(date)-new Date(fx.date))/(1000*60*60*24) > 1){
+            var dates           = _.pluck(res.data, "date"    )
+              , fxEurUsd        = _.filter(res.data, function(fx){ return fx.cur_from === "EUR" && fx.cur_to === "USD" })[0]
+              , validDates      = _.reduce(dates, function(memo, date_obs){
+                return memo && (new Date(date_exp) - new Date(date_obs))/(1000*60*60*24) === 0
+              }, true)
+
+            if(!validDates){
               done(new Error("Did not return the custom 'date'"))
-            } else if(!_.isNumber(fx.rate)) {
-              done(new Error("Did not return a number"))
-            } else if(fx.rate !== 1.2141) {
-              console.log(res)
+            } else if(fxEurUsd.rate !== 1.2141) {
               done(new Error("Did not return the correct EUR/USD FX rate"))
             } else {
               done()
