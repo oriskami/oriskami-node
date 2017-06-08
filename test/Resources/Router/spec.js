3f94ed4acdc7d8252d4251a348b01ed6376f5ac8
Fabrice Colas <fcolas@ubivar.com>
2017-06-08 16:59:24 +0200

    Add Router w/ func. tests to retrieve many resources in one req.

---
+++ test/Resources/Router/spec.js
@@ -0,0 +1,53 @@
+var _                 = require("lodash")
+  , expect            = require("chai").expect
+  , ubivar            = require("../../ubivar")
+  , methods           = ["retrieve", "list"]
+
+describe("Router", function(){
+  describe("Properties", function(){
+    it("Should have a name and path attribute", function() {
+      expect(ubivar["Router"]["path"]).to.exist
+    })
+
+    it("Should link to parent (ubivar)", function() {
+      expect(ubivar["Router"]["ubivar"]).to.exist
+    })
+
+    _.each(methods, function(method){
+      var METHOD      = method.toUpperCase()
+      it("Should have "+METHOD+" methods", function(done) {
+        if(!_.isFunction(ubivar["Router"][method])){
+          return done(new Error("Should have "+METHOD+" methods"))
+        }
+        done()
+      })
+    })
+  })
+
+  describe("Methods", function(){
+    it("Should retrieve", function(done){
+      ubivar["Router"].retrieve(0, function(err, res){
+        var isStatusOk = res.statusCode >= 200 && res.statusCode <= 204
+        if(!err && res.data.length >= 0){
+          done()
+        } else {
+          done(new Error("Did not retrieve"))
+        }
+      })
+    })
+
+    it("Should list", function(done){
+      ubivar["Router"].list(function(err, res){
+        if(err) {
+          console.log(err, res)
+          done(new Error("Did not list")) 
+        } else if(!err && res.data.length === 1) {
+          done()
+        } else {
+          console.log(res)
+          done(new Error("Should have only one returned element")) 
+        }
+      })
+    })
+  })
+})
