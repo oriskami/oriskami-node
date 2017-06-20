8cd2d90c21c36325157a943d6b017de773347bd0
Fabrice Colas <fcolas@ubivar.com>
2017-06-20 13:31:08 +0200

    Add RouterDataFeature resource

---
+++ test/Resources/RouterDataFeature/spec.js
@@ -0,0 +1,40 @@
+var _                 = require("lodash")
+  , expect            = require("chai").expect
+  , ubivar            = require("../../ubivar")
+  , methods           = ["list"]
+
+describe("RouterDataFeature", function(){
+  describe("Properties", function(){
+    it("Should have a name and path attribute", function() {
+      expect(ubivar["RouterDataFeature"]["path"]).to.exist
+    })
+
+    it("Should link to parent (ubivar)", function() {
+      expect(ubivar["RouterDataFeature"]["ubivar"]).to.exist
+    })
+
+    _.each(methods, function(method){
+      var METHOD      = method.toUpperCase()
+      it("Should have "+METHOD+" methods", function(done) {
+        if(!_.isFunction(ubivar["RouterDataFeature"][method])){
+          return done(new Error("Should have "+METHOD+" methods"))
+        }
+        done()
+      })
+    })
+  })
+
+
+  describe("Methods", function(){
+    it.skip("Should list", function(done){
+      ubivar["RouterDataFeature"].list(function(err, res){
+        if(err) {
+          console.log(err, res)
+          done(err) 
+        } else {
+          done()
+        }
+      })
+    })
+  })
+})
